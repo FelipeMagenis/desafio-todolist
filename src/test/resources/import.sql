27f56a2de9116184d07e9d528f523f553182880a
Felipe Magenis <133271953+FelipeMagenis@users.noreply.github.com>
2024-05-06 18:37:39 -0300

    Implementes H2 database for Tests and Update pom.xml

---
+++ src/test/resources/import.sql
@@ -0,0 +1,12 @@
+DELETE FROM todos;
+
+INSERT INTO todos (id, nome, descricao, realizado, prioridade)
+ VALUES (9995, '@FelipeMagenis', 'Curtir', false, 1);
+INSERT INTO todos (id, nome, descricao, realizado, prioridade)
+ VALUES (9996, '@FelipeMagenis', 'Comentar', false, 1);
+INSERT INTO todos (id, nome, descricao, realizado, prioridade)
+ VALUES (9997, '@FelipeMagenis', 'Compartilhar', false, 1);
+INSERT INTO todos (id, nome, descricao, realizado, prioridade)
+ VALUES (9998, '@FelipeMagenis', 'Se Inscrever', false, 1);
+INSERT INTO todos (id, nome, descricao, realizado, prioridade)
+ VALUES (9999, '@FelipeMagenis', 'Ativar as Notificações', false, 1);
